d6fa86f3a0ca7b8fa3c006cdad8c277e64a49805
Juan Edi <juan@noredink.com>
2017-09-24 13:59:20 -0300

    Support for toggling between test and target file

--- elm-test.el
+++ elm-test.el
@@ -43,7 +43,14 @@
   :type 'string
   :group 'elm-test)
 
-(defvar elm-test-find-project-root 'elm-test--project-root)
+(defcustom elm-test-preferred-test-suffix "Test"
+  "Preferred suffix for test files. Useful if, for example, you prefer the tests
+for module 'Foo' to live in 'FooSpecs' instead of 'FooTest'."
+  :type 'string
+  :group 'elm-test)
+
+(defvar elm-test-run-directory-for-file 'elm-test-standard-project-root-for-file)
+(defvar elm-test-project-root-for-file 'elm-test-standard-project-root-for-file)
 
 (define-compilation-mode elm-test-compilation-mode "Elm Test Compilation"
   "Compilation mode for elm-test output."
@@ -64,6 +71,106 @@
    (buffer-file-name)
    (concat elm-test-command-options " --watch")))
 
+(defun elm-test-toggle-test-and-target ()
+  "Switch to the test or the target file for the current buffer.
+If the current buffer is visiting a test file, switches to the
+target, otherwise the test."
+  (interactive)
+  (find-file (elm-test--test-or-target)))
+
+(defun elm-test--test-or-target ()
+  (if (elm-test-buffer-is-test-p)
+      (elm-test-target-file-for (buffer-file-name))
+    (elm-test-test-file-for (buffer-file-name))))
+
+;;;###autoload
+(defun elm-test-buffer-is-test-p ()
+  "Return true if the current buffer is a test."
+  (and (buffer-file-name)
+       (elm-test-test-file-p (buffer-file-name))))
+
+(defun elm-test-test-file-p (a-file-name)
+  "Return true if the specified A-FILE-NAME is a test."
+  (numberp (string-match
+            (concat elm-test-preferred-test-suffix "\\.elm\\'")
+            a-file-name)))
+
+(defun elm-test-test-file-for (a-file-name)
+  "Find test for the specified file."
+  (if (elm-test-test-file-p a-file-name)
+      a-file-name
+    ;; This replace-regex accounts for target-files living in "holder"
+    ;; directories such as "src" (since we don't want that part to appear in the
+    ;; expected test file name).
+    (let ((replace-regex (if (elm-test-target-in-holder-dir-p a-file-name)
+                             "^\\.\\./[^/]+/"
+                           "^\\.\\./"))
+          (relative-file-name (file-relative-name a-file-name (elm-test-test-directory a-file-name))))
+      (elm-test-testize-file-name (expand-file-name (replace-regexp-in-string replace-regex "" relative-file-name)
+                                                 (elm-test-test-directory a-file-name))))))
+
+(defun elm-test-target-in-holder-dir-p (a-file-name)
+  ;; tells if A-FILE-NAME is contained in one of the "well known" source
+  ;; directories (that would be only "./src" for the moment)
+  (string-match (concat "^" (concat
+                             (regexp-quote (elm-test-project-root a-file-name))
+                             "src"
+                             "/"))
+                a-file-name))
+
+(defun elm-test-target-file-for (a-spec-file-name)
+  "Find the target for A-SPEC-FILE-NAME."
+  (let ((candidate (elm-test-targetize-file-name a-spec-file-name)))
+    (cl-loop for dir in (list "." "src")
+             for target = (replace-regexp-in-string
+                           "/tests/"
+                           (concat "/" dir "/")
+                           candidate)
+             if (file-exists-p target)
+             return target)))
+
+(defun elm-test-parent-directory (a-directory)
+  "Returns the directory of which A-DIRECTORY is a child"
+  (file-name-directory (directory-file-name a-directory)))
+
+(defun elm-test-root-directory-p (a-directory)
+  "Return t if A-DIRECTORY is the root."
+  (equal a-directory (elm-test-parent-directory a-directory)))
+
+(defun elm-test-test-directory (a-file)
+  "Return the nearest spec directory that could contain specs for A-FILE."
+  (if (file-directory-p a-file)
+      (or
+       (car (directory-files a-file t "^tests$"))
+       (if (elm-test-root-directory-p a-file)
+           nil
+         (elm-test-test-directory (elm-test-parent-directory a-file))))
+    (elm-test-test-directory (elm-test-parent-directory a-file))))
+
+(defun elm-test-testize-file-name (a-file-name)
+  "Return A-FILE-NAME but converted in to a test file name."
+  (concat
+   (file-name-directory a-file-name)
+   (replace-regexp-in-string
+    "\\.elm$"
+    (concat elm-test-preferred-test-suffix ".elm")
+    (file-name-nondirectory a-file-name))))
+
+(defun elm-test-targetize-file-name (a-file-name)
+  "Return A-FILE-NAME but converted into a non-test file name with EXTENSION."
+  (concat (file-name-directory a-file-name)
+          (elm-test-file-name-with-extension
+           (replace-regexp-in-string
+            (concat elm-test-preferred-test-suffix "\\.elm")
+            ".elm"
+            (file-name-nondirectory a-file-name)))))
+
+(defun elm-test-file-name-with-extension (a-file-name)
+  "Add .elm file extension to A-FILE-NAME if it does not already have an extension."
+  (if (file-name-extension a-file-name)
+      a-file-name ;; file has a extension already so do nothing
+    (concat a-file-name ".elm")))
+
 (defun elm-test--run-single-file (test-file &rest opts)
   "Run elm-test on SPEC_FILE with the specified options OPTS."
   (elm-test--compile (shell-quote-argument test-file) opts))
@@ -74,8 +181,8 @@
   (setq elm-test-last-directory default-directory
         elm-test-last-arguments (list target opts))
 
-  (let ((default-directory (or (apply elm-test-find-project-root ())
-                               (elm-test--project-root)
+  (let ((default-directory (or (elm-test-run-directory)
+                               (elm-test-standard-project-root-for-file (buffer-file-name))
                                default-directory)))
     (compile
      (elm-test--compile-command target opts)
@@ -89,15 +196,32 @@
     (let ((default-directory elm-test-last-directory))
       (apply #'elm-test--compile elm-test-last-arguments))))
 
-(defun elm-test--project-root ()
-  "Looks for the root of an elm project, assuming a directory structure in which
-the root contains a 'test' directory for elm-test stuff.
-It works by going up the directory hierarchy (starting at the current file)
-until we reach the first elm-package.json file (which would correspond to tests)
-and going up one more time to reach the root."
-  (let
-      ((elm-test-directory (locate-dominating-file (buffer-file-name) "elm-package.json")))
-    (file-name-directory (directory-file-name elm-test-directory))))
+(defun elm-test-run-directory (&optional current-file-name)
+  (let*
+      ((starting-point (or current-file-name (buffer-file-name)))
+       (root-dir (apply elm-test-run-directory-for-file (list starting-point))))
+    (expand-file-name root-dir)))
+
+(defun elm-test-project-root (&optional current-file-name)
+  (let*
+      ((starting-point (or current-file-name (buffer-file-name)))
+       (root-dir (apply elm-test-project-root-for-file (list starting-point))))
+    (expand-file-name root-dir)))
+
+(defun elm-test-standard-project-root-for-file (current-file-name)
+  "Looks for the root of an elm project. That is, the one with the main elm-package.json
+This assumes a directory structure in which the root contains a 'tests' directory
+for elm-test stuff."
+  ;; If we are on a target file, return the first directory we see with an elm-package.json
+  ;; If we are on a test file, it's the one above.
+  (let*
+      ((first-elm-package-dir (locate-dominating-file current-file-name "elm-package.json"))
+       (parent-dir (elm-test-parent-directory first-elm-package-dir))
+       (parent-dir-elm-package (car (directory-files parent-dir t "^elm-package\.json$"))))
+
+    (if parent-dir-elm-package
+        parent-dir
+        first-elm-package-dir)))
 
 (defun elm-test--compile-command (target &optional opts)
   "Composes elm-test command line for the compile function"
